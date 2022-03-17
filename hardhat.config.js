6428948397c5679f79f30e22e995a677287af150
nahomgirma <nahotigirma63@gmail.com>
2022-03-17 12:10:37 +0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() 
+//|| "01234567890123456789";
+//const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const infuraId = "37cbac69f35247a6b09b5c7712f52d30";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,20 +11,20 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
+    
     mumbai: {
       // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
       url: "https://rpc-mumbai.matic.today",
       accounts: [privateKey]
     },
     matic: {
       // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
-    */
+    
   },
   solidity: {
     version: "0.8.4",
